7abac02dee8e60de336d7f8f75e8c24fe1ac5b1d
Megan Sawkar <mwelch122@gmail.com>
2016-10-30 23:52:44 -0700

    October 30th Update: Checkpoint_4

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -13,15 +13,38 @@ class WikisController < ApplicationController
     end
   end
 
+  def show
+    @wiki = Wiki.find(params[:id])
+  end
+
   def update
+    @wiki = Wiki.find(params[:id])
+    @wiki.assign_attributes(wiki_params)
+
+    if @wiki.save
+      flash[:notice] = "Wiki was updated successfully."
+      redirect_to root_path
+    else
+      flash.now[:alert] = "There was an error saving the wiki. Please try again."
+      redirect_to root path
+    end
   end
 
   def destroy
+    @wiki = Wiki.find(params[:id])
+
+    if @wiki.destroy
+      flash[:notice] = "\"#{@wiki.title}\" was deleted successfully."
+      redirect_to root_path
+    else
+      flash.now[:alert] = "There was an error deleting the post."
+      redirect_to root_path
+    end 
   end
 
   private
 
   def wiki_params
-    params.require(:wiki).permit(:title, :body, :user_id, :private)
-  end 
+    params.require(:wiki).permit(:title, :body, :private)
+  end
 end
--- spec/controllers/wikis_controller_spec.rb
+++ spec/controllers/wikis_controller_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe WikisController, type: :controller do
+  let(:user) { User.create!(email: "mwelch122@gmail.com", password: "password")}
+  let(:my_wiki) { Wiki.create!(title: "New Wiki Title", body: "New Wiki Body", user: user, private: false)}
 
   describe "GET #create" do
     it "returns http success" do
