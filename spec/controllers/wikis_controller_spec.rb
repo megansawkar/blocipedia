318502691929be5d60e292284be7f374e70e535d
Megan Sawkar <mwelch122@gmail.com>
2016-11-02 13:22:39 -0700

    Checkpoint 4: Wiki_Controller_Spec Update

--- spec/controllers/wikis_controller_spec.rb
+++ spec/controllers/wikis_controller_spec.rb
@@ -13,6 +13,24 @@ context "member user doing CRUD on a wiki they down" do
       sign_in @user
     end
 
+    describe "GET show" do
+        it "returns http success" do
+          get :show, id: my_wiki.id
+          expect(response).to have_http_status(:success)
+        end
+
+        it "renders the #show view" do
+          get :show, id: my_wiki.id
+          expect(response).to render_template :show
+        end
+
+        it "assigns my_post to @post" do
+          get :show, id: my_wiki.id
+          expect(assigns(:wiki)).to eq(my_wiki)
+        end
+      end
+
+
   describe "GET new" do
     it "returns http success" do
       get :new
@@ -45,6 +63,63 @@ context "member user doing CRUD on a wiki they down" do
       expect(response).to redirect_to Wiki.last
     end
   end
+
+  describe "GET edit" do
+     it "returns http success" do
+       get :edit, {id: my_wiki.id}
+       expect(response).to have_http_status(:success)
+     end
+
+     it "renders the #edit view" do
+       get :edit, {id: my_wiki.id}
+       expect(response).to render_template :edit
+     end
+
+     it "assigns wiki to be updated to @wiki" do
+       get :edit, {id: my_wiki.id}
+
+       wiki_instance = assigns(:wiki)
+
+       expect(wiki_instance.id).to eq my_wiki.id
+       expect(wiki_instance.title).to eq my_wiki.title
+       expect(wiki_instance.body).to eq my_wiki.body
+     end
+   end
+
+   describe "PUT update" do
+     it "updates wiki with expected attributes" do
+       new_title = Faker::Lorem.sentence
+       new_body = Faker::Lorem.paragraph
+
+       put :update, id: my_wiki.id, wiki: {title: new_title, body: new_body}
+
+       updated_wiki = assigns(:wiki)
+       expect(updated_wiki.id).to eq my_wiki.id
+       expect(updated_wiki.title).to eq new_title
+       expect(updated_wiki.body).to eq new_body
+     end
+
+     it "redirects to the updated wiki" do
+       new_title = Faker::Lorem.sentence
+       new_body = Faker::Lorem.paragraph
+
+       put :update, id: my_wiki.id, wiki: {title: new_title, body: new_body}
+       expect(response).to redirect_to my_wiki
+     end
+   end
+
+   describe "DELETE destroy" do
+     it "deletes the wiki" do
+       delete :destroy, {id: my_wiki.id}
+       count = Wiki.where({id: my_wiki.id}).size
+       expect(count).to eq 0
+     end
+
+     it "redirects to posts index" do
+       delete :destroy, {id: my_wiki.id}
+       expect(response).to redirect_to wikis_path
+     end
+   end
 end
 
   #context "guest" do
